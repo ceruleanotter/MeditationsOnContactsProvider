2e6c8c8b08fb3f478d34d24548e29f42350240b2
Lyla <lyla@udacity.com>
2016-03-18 14:03:42 -0700

    FEAT: Print out all emails with ID

--- app/src/main/java/com/udacity/example/emergencycontacts/MainActivity.java
+++ app/src/main/java/com/udacity/example/emergencycontacts/MainActivity.java
@@ -1,15 +1,77 @@
 package com.udacity.example.emergencycontacts;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Cursor mCursor;
+    TextView mOutputTextView;
+    // Request code for READ_CONTACTS. It can be any number > 0.
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mOutputTextView = (TextView) findViewById(R.id.contactsList);
 
-        
+        // Read and show the contacts
+        showContacts();
+    }
+
+
+        /**
+         * Show the contacts in the ListView.
+         */
+    private void showContacts() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        } else {
+            // Android version is lesser than 6.0 or the permission is already granted.
+
+            Uri uri = ContactsContract.Contacts.CONTENT_URI;
+            String[] projection = new String[]{
+                    ContactsContract.Contacts._ID,
+                    ContactsContract.Contacts.DISPLAY_NAME
+            };
+
+            mCursor = getContentResolver().query(
+                    uri,   // The content URI of the words table
+                    projection,                        // The columns to return for each row
+                    null,                    // Selection criteria
+                    null,                     // Selection criteria
+                    null);                        // The sort order for the returned rows
+
+            while (mCursor.moveToNext()) {
+                String id = mCursor.getString(0);
+                String name = mCursor.getString(1);
+
+                mOutputTextView.setText(mOutputTextView.getText() + "\n" + id + " : " + name);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                showContacts();
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
